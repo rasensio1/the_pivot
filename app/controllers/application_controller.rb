02877b102027c255c0e3d02ae1fd5fa4f9e32744
Ryan Asensio <Ryan.asensio@gmail.com>
2015-10-14 17:41:47 -0600

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :cart, :current_admin, :dollars
+  helper_method :current_user, :cart, :current_admin
 
   def cart
     @cart ||= Cart.new(session[:cart])
