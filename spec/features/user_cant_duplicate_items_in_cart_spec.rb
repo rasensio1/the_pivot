09cc51fa42a61961d19da40627cc861cae47c73b
Alon Waisman <mowalon@yahoo.com>
2015-10-15 13:08:44 -0600

    Changed tests.
    Changed the quantity adjustment test to test AGAINST changing quantity. Also fixed guest_can_add_items test

---
+++ spec/features/user_cant_duplicate_items_in_cart_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.describe "Adding photos to cart: ", type: :feature do
+  fixtures :users
+  fixtures :stores
+  fixtures :photos
+
+  it "can't add the same photo twice" do
+    visit photos_path
+
+    2.times do
+      first(:button, "Add to Cart").click
+    end
+
+    expect(current_path).to eq photos_path
+    expect(page).to have_content "already in your cart"
+    expect(page).to_not have_content "Successfully added"
+  end
+
+end
