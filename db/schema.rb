e662f8276e98dae5b870650cd9d3914e22325c2e
Jason Wright <noobjey@gmail.com>
2015-10-14 22:44:00 -0600

    carrier wave storing file locally

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151014220056) do
+ActiveRecord::Schema.define(version: 20151015021052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,9 +60,18 @@ ActiveRecord::Schema.define(version: 20151014220056) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.integer  "store_id"
+    t.string   "file"
   end
 
   add_index "photos", ["store_id"], name: "index_photos_on_store_id", using: :btree
+
+  create_table "pics", force: :cascade do |t|
+    t.string   "name"
+    t.string   "url"
+    t.string   "file"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "statuses", force: :cascade do |t|
     t.string   "name"
