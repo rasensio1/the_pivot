cadddd2d5bd6c755a9397c77adf8e5b76597fb83
Ryan Asensio <ryan.asensio@gmail.com>
2015-10-14 18:58:36 -0600

    style new business page

--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Store, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid without a name" do
+  end
+
+  it "is invalid without a tagline" do
+  end
 end
