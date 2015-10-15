9a8af4768a6a8a087fd38ce0126443c70f3cc82f
Ryan Asensio <Ryan.asensio@gmail.com>
2015-10-15 17:52:59 -0600

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :cart, :current_admin, :current_store
+  helper_method :current_user, :cart, :current_admin
 
   def cart
     @cart ||= Cart.new(session[:cart])
@@ -19,10 +19,6 @@ class ApplicationController < ActionController::Base
 
   def authorization_error
     render file: "/public/404_authorization.html", layout: false, status: 404
-  end
-
-  def current_store
-    byebug
   end
 
 end
