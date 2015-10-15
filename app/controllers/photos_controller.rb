1cc360053173eb6250d7e9ad412963a49308a681
Ryan Asensio <ryan.asensio@gmail.com>
2015-10-15 17:28:58 -0600

    adds slug and namespaced photo indexes for a store

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,9 +1,13 @@
 class PhotosController < ApplicationController
   def index
-    @photos = Photo.where(status: 0).paginate(page: params[:page])
+    @photos = Store.find_by(slug: params[:store_name]).photos.where(status: 0).paginate(page: params[:page])
   end
 
   def show
     @photo = Photo.find(params[:id])
+  end
+  
+  def all
+    @photos = Photo.where(status: 0).paginate(page: params[:page])
   end
 end
---
+++ spec/features/guest_views_shop_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe "a guest", type: :feature do
+  fixtures :stores
+  fixtures :users
+
+  it "can visit a store from the correct url" do
+    alon = User.find_by(name: "Alon Example")
+    store = Store.create(name: "alonstore", tagline: "my store", user_id: alon.id)
+    photo = Photo.create(title: "AlonPhoto", description: "yeah", standard_price: 100, store_id: store.id)
+    sign_in(alon)
+
+    visit "/alonstore/photos"
+
+    save_and_open_page
+
+    expect(page).to have_content("AlonPhoto")
+    expect(page).to_not have_content("Example Title")
+  end
+
+end
