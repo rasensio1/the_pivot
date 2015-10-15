35be90eb0e1bef06ab5db5a62eba407e5e151942
Alon Waisman <mowalon@yahoo.com>
2015-10-14 20:10:34 -0600

    Paginate store show page
    Need to pass in @photos for store show page in order to support pagination.

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -18,7 +18,7 @@ class StoresController < ApplicationController
 
   def show
     @store = Store.find(params[:id])
-    @photos = @store.photos
+    @photos = @store.photos.paginate(page: params[:page])
   end
 
   private
