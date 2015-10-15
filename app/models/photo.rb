3e7d66c77beedd40c8780e01df54c6ef87c8b2af
Ryan Asensio <ryan.asensio@gmail.com>
2015-10-15 12:18:18 -0600

    adds model tests for photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,9 +1,9 @@
 class Photo < ActiveRecord::Base
   belongs_to :store
-
   mount_uploader :file, PhotoUploader
-
   enum status: %w(active inactive)
+
+  validates :title, :description, :standard_price, :store_id,  presence: true 
 
   def shortened_description
     description.length > 60 ? (description[0..60] + "...") : description
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Photo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :photos
+
+    let(:photo) { Photo.first }
+
+  it "is invalid without a title" do
+    photo.update(title: nil)
+
+    expect(photo).to_not be_valid
+  end
+  it "is invalid without a description" do
+    photo.update(description: nil)
+
+    expect(photo).to_not be_valid
+  end
+  it "is invalid without a standard price" do
+    photo.update(standard_price: nil)
+
+    expect(photo).to_not be_valid
+  end
+  it "is invalid without a store id" do
+    photo.update(store_id: nil)
+
+    expect(photo).to_not be_valid
+  end
 end
