789cc1af836992a5162fd31765a9594818886ac8
Ryan Asensio <ryan.asensio@gmail.com>
2015-10-15 18:50:42 -0600

    write test for adding other users as admins to a shop

---
+++ spec/features/shop_owner_adds_shop_admins_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.describe "an admin on their dashboards" do
+
+  it "can add other admins for her shop" do
+    admin = User.create(name: "Admin", email: "admin@yeah.com", password: "password")
+    ryan = User.create(name: "Regular Ryan", email: "ryan@yeah.com", password: "password")
+    store = Store.create(name: "The Store", tagline: "For everyone", user_id: admin.id)
+    sign_in(admin)
+
+    visit edit_admin_store_path(store)
+
+    fill_in("store[add_admin]", with: "ryan@yeah.com")
+    click_on "Add Admin"
+
+    expect(page).to have_content("Regular Ryan")
+    expect(page).to have_content("ryan@yeah.com")
+
+    click_on "Sign Out"
+
+    sign_in(ryan)
+
+    expect(page).to have_content("Admin for")
+    expect(page).to have_content("The Store")
+  end
+end
