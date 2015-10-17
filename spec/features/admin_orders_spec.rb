1d3ecce7495a72a8927b668c418bc3856ef4f58f
Jason Wright <noobjey@gmail.com>
2015-10-17 16:59:32 -0600

    admin can see their purchases and details

--- spec/features/admin_orders_spec.rb
+++ spec/features/admin_orders_spec.rb
@@ -1,26 +1,25 @@
 require "rails_helper"
 
-
-
-# An admin user should have a profile link and a dashboard link, make them go rght places and test
-# Hardcoded fixture values for money in tests, think of something better maybe
-# Add a view button to orders table on profile page
-
-
 RSpec.describe "an admin" do
   fixtures :users
   fixtures :stores
   fixtures :photos
   fixtures :orders
   fixtures :order_items
+  fixtures :statuses
 
   let!(:store_admin) { User.find_by(name: "admin") }
+  let!(:other_store) { Store.find_by(name: "Alons Store")
+  }
   let!(:order1) { store_admin.orders.first }
   let!(:order2) { store_admin.orders.last }
+  let!(:admin_order_item_1) { order2.order_items.first }
+  let!(:admin_order_item_2) { order2.order_items.last }
 
   context "visits profile" do
     before(:each) do
       sign_in(store_admin)
+      visit(profile_path(store_admin))
     end
 
     it "can see all orders" do
@@ -35,111 +34,28 @@ RSpec.describe "an admin" do
         find("td", text: order1.id)
         find("td", text: "$23.00")
         first("td", text: order1.order_items.count)
-
         find("td", text: order2.id)
-        find("td", text: "$87.34")
+        find("td", text: "170.75")
       end
 
     end
 
-    xit "can click a link to view an order" do
-      click_link "View"
+    it "can view order details" do
+      within("#order-show-#{order2.id}") do
+        click_link("View")
+      end
 
-      expect(current_path).to eq("/orders/#{order.id}")
-      expect(page).to have_content("Order Number: #{order.id}")
-    end
+      expect(current_path).to eq("/orders/#{order2.id}")
+      expect(page).to have_content("Order Number: #{order2.id}")
+      expect(page).to have_content("Total: $170.75")
 
-    xit "can see ordered orders" do
-      expect(page).to have_content(ordered_status.name)
-      expect(page).to have_content(order.id)
-      expect(page).to have_content(order.total)
-      expect(page).to have_content(order.status)
-    end
+      find_link(admin_order_item_1.photo.title).visible?
+      expect(page).to have_content(admin_order_item_1.photo.description)
+      expect(page).to have_content("$87.34")
 
-    xit "can see paid orders" do
-      expect(page).to have_content(paid_status.name)
-      expect(page).to have_content(order.id)
-      expect(page).to have_content(order.total)
-      expect(page).to have_content(order.status)
-    end
-
-    xit "can see cancelled orders" do
-      expect(page).to have_content(cancelled_status.name)
-      expect(page).to have_content(order.id)
-      expect(page).to have_content(order.total)
-      expect(page).to have_content(order.status)
-    end
-
-    xit "can see completed orders" do
-      expect(page).to have_content(completed_status.name)
-      expect(page).to have_content(order.id)
-      expect(page).to have_content(order.total)
-      expect(page).to have_content(order.status)
-    end
-
-    xit "can see the total number of orders for all types" do
-      orders = Order.all
-
-      expect(page).to have_content(orders.count)
-    end
-
-    xit "can see the total number of ordered orders" do
-      orders = Order.where(status_id: ordered_status.id)
-
-      expect(page).to have_content(orders.count)
-    end
-
-    xit "can see the total number of orders for paid orders" do
-      orders = Order.where(status_id: paid_status.id)
-
-      expect(page).to have_content(orders.count)
-    end
-
-    xit "can see the total number of orders for cancelled orders" do
-      orders = Order.where(status_id: cancelled_status.id)
-
-      expect(page).to have_content(orders.count)
-    end
-
-    xit "can see the total number of orders for completed orders" do
-      orders = Order.where(status_id: completed_status.id)
-
-      expect(page).to have_content(orders.count)
-    end
-
-    xit "can change the status for an individual order if the status is paid" do
-      order.update_attributes(status_id: paid_status.id)
-      click_link "View"
-
-      expect(page).to have_content(paid_status.name)
-      select "Completed", from: "order[status]"
-      click_button "Update Order Status"
-      expect(current_path).to eq(admin_orders_path)
-      expect(page).to have_content(completed_status.name)
-    end
-
-    xit "can change the status for an individual order if status is ordered" do
-      order.update_attributes(status_id: ordered_status.id)
-      click_link "View"
-
-      expect(page).to have_content(ordered_status.name)
-      select "Paid", from: "order[status]"
-      click_button "Update Order Status"
-      expect(current_path).to eq(admin_orders_path)
-      expect(page).to have_content(paid_status.name)
-    end
-
-
-    xit "updating the status changes the status attribute in the database" do
-      order.update_attributes(status_id: ordered_status.id)
-      click_link "View"
-
-      select "Paid", from: "order[status]"
-      click_button "Update Order Status"
-
-
-      expect(current_path).to eq(admin_orders_path)
-      expect(page).to have_content "Paid Total: 1"
+      find_link(admin_order_item_2.photo.title).visible?
+      expect(page).to have_content(admin_order_item_2.photo.description)
+      expect(page).to have_content("$83.41")
     end
   end
 end
