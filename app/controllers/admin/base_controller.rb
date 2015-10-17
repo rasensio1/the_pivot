560a61cd29255c3471dcde76a0e3fcc8dc039205
Jason Wright <noobjey@gmail.com>
2015-10-17 00:17:17 -0600

    updated so the store owner can also edit the store

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,6 +1,12 @@
 class Admin::BaseController < ApplicationController
 
   def require_shop_admin
-    authorization_error unless current_user.stores.pluck(:slug).include?(params[:store_name])
+    authorization_error unless store_admin?
+  end
+
+  private
+
+  def store_admin?
+    current_user.stores.pluck(:slug).include?(params[:store_name]) || current_user.store
   end
 end
