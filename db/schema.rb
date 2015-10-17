237194b759b98e6767fa1d31c4378d51839315f8
Ryan Asensio <ryan.asensio@gmail.com>
2015-10-17 15:42:14 -0600

    drops items table

--- db/schema.rb
+++ db/schema.rb
@@ -11,24 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151017154412) do
+ActiveRecord::Schema.define(version: 20151017214131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
-  end
-
-  create_table "items", force: :cascade do |t|
-    t.string   "name"
-    t.string   "description"
-    t.decimal  "price",       precision: 10, scale: 2
-    t.datetime "created_at",                                       null: false
-    t.datetime "updated_at",                                       null: false
-    t.string   "image_url"
-    t.integer  "category_id"
-    t.integer  "status",                               default: 0
   end
 
   create_table "order_items", force: :cascade do |t|
