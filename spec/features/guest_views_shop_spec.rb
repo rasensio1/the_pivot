f2dbcb436c7790a2bda650522cc3f9e0716ecadd
Jason Wright <noobjey@gmail.com>
2015-10-17 15:47:09 -0600

    Update guest_views_shop_spec.rb
    removed save, and requirement for user logging in

--- spec/features/guest_views_shop_spec.rb
+++ spec/features/guest_views_shop_spec.rb
@@ -10,10 +10,7 @@ RSpec.describe "a guest", type: :feature do
     store = Store.create(name: "alonstore", tagline: "my store", user_id: alon.id)
     photo = Photo.update(Photo.first.id, title: "AlonPhoto", description: "yeah", standard_price: 100, store_id: store.id)
 
-    sign_in(User.first)
-
     visit store_photos_path(store.slug) 
-    save_and_open_page
 
     expect(page).to have_content("AlonPhoto")
     expect(page).to_not have_content("Example Title")
