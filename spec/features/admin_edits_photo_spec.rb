d72c17b0dd93fa40278c8d1e992bd2c7f6cb3697
Jason Wright <noobjey@gmail.com>
2015-10-17 17:10:46 -0600

    fixing a merge mistake that erase this file

--- spec/features/admin_edits_photo_spec.rb
+++ spec/features/admin_edits_photo_spec.rb
@@ -3,68 +3,94 @@ require "rails_helper"
 RSpec.describe "a store admin" do
   fixtures :users
   fixtures :stores
-  # fixtures :photos
-
+  fixtures :photos
 
   let!(:admin) { User.find_by(name: "admin") }
-  # let!(:photo) { admin.store.photos.first }
+  let!(:store) { admin.store }
+  let!(:photo) { admin.store.photos.first }
 
 
   context "visits photo management page" do
     before do
-      # sign_in(admin)
-      # expect(current_path).to be(profile_path)
-      # visit edit_admin_store_path
+      sign_in(admin)
+      visit admin_store_path(store.slug)
     end
 
     context "clicks the Edit link" do
       before do
-        # click_link "Edit"
+        click_link "Edit"
       end
 
-      it "a test" do
-        expect(true).to be(false)
+      it "sees the edit form for that photo" do
+        expect(current_path).to eq(edit_store_photo_path(store.slug, photo))
+
+        expect(page).to have_content("Edit Photo")
+        expect(page).to have_selector("input[value='#{photo.title}']")
+        expect(page).to have_selector("input[value='#{photo.title}']")
+        expect(page).to have_selector("input[value='#{photo.description}']")
+        expect(page).to have_selector("input[value='$23.00']")
+        expect(page).to have_selector("input[value='$35.00']")
       end
 
-      xit "shows the edit page for that item" do
-        expect(current_path).to eq("/admin/items/#{item.id}/edit")
+      it "shows store admin page after submit" do
+        fill_in "Title", with: "New Title"
+        click_button "Submit"
+
+        expect(current_path).to eq(admin_store_path(store.slug))
       end
 
-      xit "updates the name" do
-        fill_in "Name", with: "New Name"
-        click_button "Update Meal"
+      it "updates the title" do
+        fill_in "Title", with: "New Title"
+        click_button "Submit"
 
-        expect(current_path).to eq(admin_items_path)
-        expect(page).to have_content("New Name")
+        within("table") do
+          expect(page).to have_content("New Title")
+        end
       end
 
-      xit "updates the description" do
+      it "updates the description" do
         fill_in "Description", with: "New Description"
-        click_button "Update Meal"
+        click_button "Submit"
 
-        expect(page).to have_content("New Description")
+        within("table") do
+          expect(page).to have_content("New Description")
+        end
       end
 
-      xit "updates the price " do
-        fill_in "Price", with: 5
-        click_button "Update Meal"
+      it "updates the standard price" do
+        fill_in "Standard price", with: 51
+        click_button "Submit"
 
-        expect(page).to have_content(5)
+        within("table") do
+          expect(page).to have_content("$51.00")
+        end
       end
 
-      xit "updates the image_url" do
-        fill_in "Image url", with: "new_image_url"
-        click_button "Update Meal"
+      it "updates the commercial price" do
+        fill_in "Commercial price", with: "$1,287.33"
+        click_button "Submit"
 
-        expect(page).to have_css("img[src*='images/new_image_url']")
+        within("table") do
+          expect(page).to have_content("$1,287.33")
+        end
       end
 
-      xit "updates the status" do
-        select "Retired", from: "item[status]"
-        click_button "Update Meal"
+      context "with invalid input params" do
+        it "is redirected to the form with error messages" do
+          fill_in "Standard price", with: "Hello"
+          click_button "Submit"
 
-        expect(page).to have_content("retired")
+          expect(current_path).to eq(edit_store_photo_path(store.slug, photo))
+          expect(page).to have_content("Standard price - Must contain")
+        end
       end
+    end
+
+    it "can go to the show page" do
+      click_link photo.title
+
+      expect(page).to have_content(photo.title)
+      expect(current_path).to eq(store_photo_path(store.slug, photo))
     end
   end
 end
