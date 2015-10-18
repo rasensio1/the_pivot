528265ebc7bec678b72bcee39bdaa925ca480039
Ryan Asensio <ryan.asensio@gmail.com>
2015-10-18 16:23:18 -0600

    changes routes to add shop_photos_path. seems like there was a weird rails bug

--- app/controllers/stores/photos_controller.rb
+++ app/controllers/stores/photos_controller.rb
@@ -3,7 +3,6 @@ class Stores::PhotosController < ApplicationController
   def index
     @store =  Store.find_by(slug: params[:store_name])
     @photos = @store.photos.active.cat_filter(filter_id).paginate(page: params[:page])
-    @categories = @photos.map(&:category).uniq
+    @categories = @store.photos.map(&:category).uniq
   end
-
 end
