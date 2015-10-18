cd5eb10adb457417ca2e6ece587373f587d892d6
Ryan Asensio <ryan.asensio@gmail.com>
2015-10-18 15:33:13 -0600

    adds test for shop show categories

--- spec/features/guest_views_shop_spec.rb
+++ spec/features/guest_views_shop_spec.rb
@@ -4,16 +4,33 @@ RSpec.describe "a guest", type: :feature do
   fixtures :stores
   fixtures :users
   fixtures :photos
+  fixtures :categories
 
-  it "can visit a store from the correct url" do
-    alon = User.find_by(email: "alon@example.ninja")
-    store = Store.create(name: "alonstore", tagline: "my store", user_id: alon.id)
-    Photo.update(Photo.first.id, title: "AlonPhoto", description: "yeah", standard_price: 100, store_id: store.id)
+  context "can visit a store" do
+    before do
+      category = Category.first
+      alon = User.find_by(name: "Alon Example")
+      store = Store.create(name: "alonstore", tagline: "my store", user_id: alon.id)
+      photo = Photo.update(Photo.first.id, title: "AlonPhoto", description: "yeah", standard_price: 100, store_id: store.id, category_id: category.id)
+      Photo.update(Photo.second.id, title: "Not here", description: "yeah", standard_price: 100, store_id: store.id, category_id: Category.last.id)
+    end
 
-    visit store_photos_path(store.slug) 
+    let(:store) { Store.find_by(name: "alonstore") }
+    let(:category) { Category.first }
 
-    expect(page).to have_content("AlonPhoto")
-    expect(page).to_not have_content("Example Title")
-  end
+    it "from the correct url" do
+      visit store_photos_path(store.slug) 
 
+      expect(page).to have_content("AlonPhoto")
+      expect(page).to_not have_content("Example Title")
+    end
+
+    it "and stort by category" do
+      visit store_photos_path(store.slug) 
+      click_link category.name
+
+      expect(page).to have_content("AlonPhoto")
+      expect(page).to_not have_content("Not here")
+    end
+  end 
 end
