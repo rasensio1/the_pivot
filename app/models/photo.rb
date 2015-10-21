adffe8ad0b85324ca6b579ddea41b1f7015e36f0
Alon Waisman <mowalon@yahoo.com>
2015-10-21 15:50:04 -0600

    Set pagination default
    Set the default to 21 instead of the normal 30.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -8,6 +8,8 @@ class Photo < ActiveRecord::Base
   validates :title, :description, :standard_price, :store_id,  presence: true
   validates :standard_price, :commercial_price, numericality: { greater_than: 0, message: "must contain only integers and be greater than 0" }
 
+  self.per_page = 21
+
   def shortened_description
     description.length > 60 ? (description[0..60] + "...") : description
   end
