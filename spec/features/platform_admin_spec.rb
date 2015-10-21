661c987a1da2f0d52be08e291a87134725ab8f70
Jason Wright <noobjey@gmail.com>
2015-10-20 23:56:45 -0600

    added database column for platform admin role

---
+++ spec/features/platform_admin_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+
+RSpec.describe "a platform admin", type: :feature do
+  fixtures :users
+  fixtures :stores
+  fixtures :store_admin
+  fixtures :categories
+  fixtures :photos
+
+  let!(:platform_admin) {User.find_by(email: "platform_admin@admin.com")}
+  let!(:store_admin) {User.find_by(email: "admin@admin.com")}
+  let!(:store) {store_admin.store}
+  let!(:photo) {store.photos.first}
+  let!(:category1) {Category.first}
+  let!(:category2) {Category.second}
+  let!(:category3) {Category.third}
+
+  it "can add a photo to any store" do
+    sign_in(platform_admin)
+
+    visit admin_store_path(store.slug)
+
+    click_on "Add a photo"
+    fill_in("photo[title]", with: photo.title)
+    fill_in("photo[description]", with: photo.description)
+    fill_in("photo[standard_price]", with: photo.standard_price)
+    fill_in("photo[commercial_price]", with: photo.commercial_price)
+    page.attach_file("photo[file]", Rails.root + "spec/fixtures/test_photo_1.jpg")
+
+    check "Lifestyle"
+    check "Architecture"
+    check "Landscape"
+
+    click_button("Create Photo")
+
+    expect(current_path).to eq(admin_store_path(store.slug))
+    expect(page).to have_content("#{photo.title} photo has been added!")
+    expect(page).to have_content(photo.title)
+  end
+end
