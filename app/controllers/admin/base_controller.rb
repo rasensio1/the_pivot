b7f88666e6dbd7afd0c0574784331cba64131283
Jason Wright <noobjey@gmail.com>
2015-10-21 10:15:44 -0600

    platform admin has rights to any page

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,12 +1,17 @@
 class Admin::BaseController < ApplicationController
 
   def require_shop_admin
-    authorization_error unless store_admin?
+    authorization_error unless store_admin? || platform_admin?
   end
+
 
   private
 
   def store_admin?
     current_user && current_user.stores.pluck(:slug).include?(params[:store_name])
+  end
+
+  def platform_admin?
+    current_user && current_user.platform_admin?
   end
 end
