5af2be215c7b413840769bcaf33730f00c8ff8ef
Jason Wright <noobjey@gmail.com>
2015-10-21 13:17:51 -0600

    platform admin goes to dashboard on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      redirect_to profile_path
+
+      redirect_to user_home_page(@user)
       flash[:success] = "YeeHaw! #{@user.name} is signed in!"
     else
       redirect_to login_path
@@ -17,5 +18,12 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     redirect_to root_path
+  end
+
+  private
+
+  def user_home_page(user)
+    return god_dashboard_path if user.platform_admin?
+    profile_path
   end
 end
