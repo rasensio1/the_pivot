928df6831d95e3fbf4e2e32e57b8cf9ff64d6663
Jason Wright <noobjey@gmail.com>
2015-10-21 21:13:53 -0600

    added active column to store

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151021052954) do
+ActiveRecord::Schema.define(version: 20151022004248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,10 +94,11 @@ ActiveRecord::Schema.define(version: 20151021052954) do
     t.string   "name"
     t.string   "tagline"
     t.integer  "user_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.string   "slug"
     t.integer  "watermark_id"
+    t.boolean  "active",       default: true
   end
 
   add_index "stores", ["user_id"], name: "index_stores_on_user_id", using: :btree
