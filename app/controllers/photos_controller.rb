7047775bf63c696f8d15d2057c86adef6641878c
Alon Waisman <mowalon@yahoo.com>
2015-10-21 23:41:51 -0600

    Disallow photo show page for inactive images
    We now redirect to the all photos page and display a flash warning page saying the photo isn't available.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,11 +2,15 @@ class PhotosController < ApplicationController
 
   def show
     @photo = Photo.find(params[:id])
+    if !@photo.active?
+      flash[:warning] = "Sorry, but the photo you're looking for isn't available."
+      redirect_to photos_path
+    end
   end
 
   def index
     @label = Category.label(filter_id)
     @photos = Photo.category_photos(filter_id).paginate(page: params[:page])
-    @categories = Category.all.sample(7)
+    @categories = Category.all.sample(8)
   end
 end
